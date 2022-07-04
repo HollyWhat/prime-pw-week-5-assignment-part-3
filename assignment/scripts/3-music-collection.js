5d9bc00d06afde35cb75f2ea8c9b65e3e1496103
Holly Drazenovich <hollydraze14@gmail.com>
2022-07-04 12:35:28 -0500

    began the add to collection function and testing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,3 +7,26 @@ console.log('***** Music Collection *****')
 // start with an empty array
 let collection = [];
 
+/**
+ * @param function that creates a new object 
+ * 
+ * @return the new album object to the collection array
+ * 
+ */
+
+
+//title, artist, year published 
+//addToCollection function start 
+
+
+
+function addToCollection(){
+    const newAlbum ={
+        title: "x",
+        artist: "test",
+        yearPublished: 2020,
+    }
+    return collection.push(newAlbum);
+}
+
+console.log("testing the add to collection", collection);
